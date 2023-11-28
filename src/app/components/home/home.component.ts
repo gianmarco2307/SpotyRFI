38a0f8c3d4e35e0b62e857aa87f12f4bfc87921b
gian <gianmarco.clu23@gmail.com>
2023-11-28 17:35:53 +0100

    Modificata la home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  ngAfterViewInit() {
+    $(function () {
+      $('[data-toggle="popover"]').popover()
+    })
+  }
 
   openLink(url: string){
     window.open(url, "_blank");
