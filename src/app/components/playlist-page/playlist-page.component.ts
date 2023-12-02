fa744036e3920cdc7d95936af73ea1c972359963
gian <gianmarco.clu23@gmail.com>
2023-12-02 12:48:28 +0100

    aggiunti commenti a playlist-page.ts

--- src/app/components/playlist-page/playlist-page.component.ts
+++ src/app/components/playlist-page/playlist-page.component.ts
@@ -11,8 +11,8 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class PlaylistPageComponent {
   id: String | null = null;
   tratte: any[] = [];
-  trattaSrc!: SafeResourceUrl;
-  srcs: SafeResourceUrl[] = [];
+  trattaSrc!: SafeResourceUrl;          //questa variabile è d'appoggio
+  srcs: SafeResourceUrl[] = [];         //array che raccoglie tutti i nostri url sanificati
 
   constructor(protected fireServ: FirebaseService, private route: ActivatedRoute, private sanitizer: DomSanitizer){}
 
@@ -20,9 +20,9 @@ export class PlaylistPageComponent {
     this.id = this.route.snapshot.paramMap.get('id');
     
     this.fireServ.getTratte().subscribe((data: any) => {
-      for(let tratta of data){
-        this.trattaSrc = this.sanitizer.bypassSecurityTrustResourceUrl(tratta.src);
-        this.srcs.push(this.trattaSrc);
+      for(let tratta of data){        //con il ciclo prende l'src di ogni "tratta" 
+        this.trattaSrc = this.sanitizer.bypassSecurityTrustResourceUrl(tratta.src);       //assegna a trattaSrc l'url sanificato
+        this.srcs.push(this.trattaSrc);           //pusho nell'array
       }
       this.tratte = data;
     })
