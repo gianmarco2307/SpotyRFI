276b877f1f844a40cda1d4119ba0e2382800a5a1
gian <gianmarco.clu23@gmail.com>
2023-12-28 14:45:46 +0100

    replaced map

--- src/app/components/playlist-page/playlist-page.component.ts
+++ src/app/components/playlist-page/playlist-page.component.ts
@@ -11,6 +11,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class PlaylistPageComponent {
   id: String | null = null;
   tratte: any[] = [];
+  stations: any[][] =  [];
   trattaSrc!: SafeResourceUrl;          //questa variabile è d'appoggio
   srcs: SafeResourceUrl[] = [];         //array che raccoglie tutti i nostri url sanificati
 
@@ -20,9 +21,15 @@ export class PlaylistPageComponent {
     this.id = this.route.snapshot.paramMap.get('id');
     
     this.fireServ.getTratte().subscribe((data: any) => {
+      let i = 0;
       for(let tratta of data){        //con il ciclo prende l'src di ogni "tratta" 
+        let temporaryStations: string[];
         this.trattaSrc = this.sanitizer.bypassSecurityTrustResourceUrl(tratta.src);       //assegna a trattaSrc l'url sanificato
         this.srcs.push(this.trattaSrc);           //pusho nell'array
+        this.stations[i] = this.stations[i] || [];
+        temporaryStations = tratta.stations.split('_');
+        this.stations[i] = temporaryStations;
+        i++;
       }
       this.tratte = data;
     })
