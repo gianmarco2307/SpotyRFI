28155a0742f14a592c02b00e5b6555b183dbd89e
gian <gianmarco.clu23@gmail.com>
2024-02-11 15:33:38 +0100

    main picture changed and updated chisiamo section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "home", component: HomeComponent },
   { path: "tratte", component: TratteComponent },
-  { path: "chisiamo", component: ChisiamoComponent },
+  { path: "chisono", component: ChisiamoComponent },
   { path: "tratta/:id", component: PlaylistPageComponent },
   { path: "**", component: NotfoundComponent }
 ];
