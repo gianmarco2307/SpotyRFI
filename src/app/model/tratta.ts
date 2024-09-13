a1307880a3ca2907b0398376bbc51e3c84ebd058
gian <gianmarco.clu23@gmail.com>
2024-09-13 16:02:32 +0200

    feat(tratta): modified stations property in interface

--- src/app/model/tratta.ts
+++ src/app/model/tratta.ts
@@ -1,10 +1,12 @@
+import { Station } from "./station";
+
 export interface Tratta {
     trattaId: string;
     partenza: string;
     destinazione: string;
     mezzo: string;
     gestore: string;
-    stations: Array<string>;
+    stations: Array<Station>;
     src: string;
     srcimg: string;
     note: string;
