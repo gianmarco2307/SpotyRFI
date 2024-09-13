692d5c3c01c4905613c487083e8b3cb5d101697c
gian <gianmarco.clu23@gmail.com>
2024-09-13 10:38:32 +0200

    removed monitor component

--- src/app/page/playlist-page/playlist-page.component.ts
+++ src/app/page/playlist-page/playlist-page.component.ts
@@ -5,14 +5,13 @@ import { NgClass, NgForOf, NgIf } from '@angular/common';
 import { TratteService } from 'src/app/services/tratte.service';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { Tratta } from 'src/app/model/tratta';
-import { MonitorComponent } from 'src/app/components/monitor/monitor.component';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 
 @Component({
   selector: 'app-playlist-page',
   standalone: true,
-  imports: [NgIf, NgForOf, NgClass, MonitorComponent, ProgressSpinnerModule],
+  imports: [NgIf, NgForOf, NgClass, ProgressSpinnerModule],
   templateUrl: './playlist-page.component.html',
   styleUrls: ['./playlist-page.component.css'],
 })
